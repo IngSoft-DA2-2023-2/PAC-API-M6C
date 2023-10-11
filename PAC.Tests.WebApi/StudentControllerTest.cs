05a003a3031835e448759b132c61db5bdc774108
FACULTADES\alumnoFI <aaasss@aaddd>
2023-10-11 09:19:21 -0300

    RED get all students

--- PAC.Tests.WebApi/StudentControllerTest.cs
+++ PAC.Tests.WebApi/StudentControllerTest.cs
@@ -7,6 +7,7 @@ using PAC.IBusinessLogic;
 using PAC.Domain;
 using PAC.WebAPI;
 using Microsoft.AspNetCore.Mvc;
+using PAC.IDataAccess;
 
 [TestClass]
 public class StudentControllerTest
@@ -17,6 +18,19 @@ public class StudentControllerTest
         [TestInitialize]
         public void InitTest()
         {
+        }
+
+        [TestMethod]
+        public void GetAllStudients()
+        {
+            List<Student> sutdients = new List<Student>();
+            var repo = new Mock<IStudentsRepository<Student>>();
+            repo.Setup(x => x.GetStudents()).Returns(sutdients);
+            var logic = new Mock<IStudentLogic>();
+            logic.Setup(x => x.GetStudents()).Returns(repo.Object.GetStudents());
+
+            var controller = new StudentController(logic.Object);
+            var result = controller.GetAllStudents();
         }
     }
 }
