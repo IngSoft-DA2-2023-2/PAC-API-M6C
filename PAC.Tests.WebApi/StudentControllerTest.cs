18056fb39b6bddb7999d2e32478407347ff722b5
Matias Olivera <matiasoliverapimienta@hotmail.com>
2023-10-11 09:47:02 -0300

    [GREEN] all user contrller metohods

--- PAC.Tests.WebApi/StudentControllerTest.cs
+++ PAC.Tests.WebApi/StudentControllerTest.cs
@@ -1,22 +1,80 @@
-﻿namespace PAC.Tests.WebApi;
-using System.Collections.ObjectModel;
-
-using System.Data;
+﻿using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PAC.IBusinessLogic;
 using PAC.Domain;
+
 using PAC.WebAPI;
-using Microsoft.AspNetCore.Mvc;
 
 [TestClass]
 public class StudentControllerTest
 {
-    [TestClass]
-    public class UsuarioControllerTest
+    private Mock<IStudentLogic> _mockStudentLogic;
+    private StudentController _studentController;
+
+    [TestInitialize]
+    public void InitTest()
     {
-        [TestInitialize]
-        public void InitTest()
+        _mockStudentLogic = new Mock<IStudentLogic>();
+        _studentController = new StudentController(_mockStudentLogic.Object);
+    }
+
+    [TestMethod]
+    public void GetAllStudentsCorrectTest()
+    {
+        
+        
+        var mockStudents = new List<Student>
         {
-        }
+            new Student { Id = 1, Name = "John" },
+            new Student { Id = 2, Name = "Jane" }
+        };
+
+        _mockStudentLogic.Setup(x => x.GetStudents()).Returns(mockStudents);
+
+        
+        var result = _studentController.GetAllStudents();
+        _mockStudentLogic.VerifyAll();
+
+
+        OkObjectResult okResult = result as OkObjectResult;
+        Assert.IsNotNull(okResult);
+        Assert.AreEqual(200, okResult.StatusCode);
+
+        var students = okResult.Value as List<Student>;
+        Assert.AreEqual(2, students.Count);
+    }
+    [TestMethod]
+    public void GetStudentByIdCorrectTest()
+    {
+        
+        var mockStudent = new Student { Id = 1, Name = "John" };
+        _mockStudentLogic.Setup(x => x.GetStudentById(1)).Returns(mockStudent);
+
+        
+        var result = _studentController.GetStudentById(1);
+
+
+        OkObjectResult okResult = result as OkObjectResult;
+        Assert.IsNotNull(okResult);
+        Assert.AreEqual(200, okResult.StatusCode);
+        var student = okResult.Value as Student;
+        Assert.AreEqual("John", student.Name);
+    }
+    [TestMethod]
+    public void CreateStudentCorrectTest()
+    {
+       
+        var newStudent = new Student { Id = 2, Name = "Jane" };
+        _mockStudentLogic.Setup(x => x.InsertStudents(newStudent));
+
+        
+        var result = _studentController.CreateStudent(newStudent);
+
+            
+        var createdResult = result as CreatedAtActionResult;
+        Assert.IsNotNull(createdResult);
+        Assert.AreEqual(201, createdResult.StatusCode);
+        var student = createdResult.Value as Student;
+        Assert.AreEqual("Jane", student.Name);
     }
 }
