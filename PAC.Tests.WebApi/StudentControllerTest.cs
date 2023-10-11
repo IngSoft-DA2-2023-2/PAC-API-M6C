7aca2302dc780b341efd5e829473b8dce2d9c8e2
PabloLoading <pabloduransc@gmail.com>
2023-10-11 10:00:44 -0300

    REFACTOR

--- PAC.Tests.WebApi/StudentControllerTest.cs
+++ PAC.Tests.WebApi/StudentControllerTest.cs
@@ -1,49 +1,46 @@
-﻿namespace PAC.Tests.WebApi;
-using System.Collections.ObjectModel;
-
-using System.Data;
-using Moq;
-using PAC.IBusinessLogic;
-using PAC.Domain;
-using PAC.WebAPI;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using PAC.BusinessLogic;
-using PAC.DataAccess;
+using Moq;
+using PAC.Domain;
+using PAC.IBusinessLogic;
+using PAC.WebAPI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-[TestClass]
-public class StudentControllerTest
+namespace PAC.Tests.WebApi
 {
     [TestClass]
-    public class UsuarioControllerTest
+    public class StudentControllerTest
     {
+        private Mock<IStudentLogic> _studentLogicMock;
+        private StudentController _controller;
+
         [TestInitialize]
         public void InitTest()
         {
+            _studentLogicMock = new Mock<IStudentLogic>();
+            _controller = new StudentController(_studentLogicMock.Object);
         }
 
         [TestMethod]
         public void CanCreateController_Ok()
         {
-            var studentLogicMock = new Mock<IStudentLogic>();
-            StudentController controller = new StudentController(studentLogicMock.Object);
-            Assert.IsNotNull(controller);
+            Assert.IsNotNull(_controller);
         }
 
         [TestMethod]
         public void CanGetAllStudents()
         {
-            var studentLogicMock = new Mock<IStudentLogic>();
             var studentList = new List<Student>
             {
                 new Student() { Id = 1, Name = "Pablo" },
                 new Student() { Id = 2, Name = "Julieta" },
                 new Student() { Id = 3, Name = "Felipe" }
             };
-            studentLogicMock.Setup(service => service.GetStudents()).Returns(studentList);
-            StudentController controller = new StudentController(studentLogicMock.Object);
 
+            _studentLogicMock.Setup(service => service.GetStudents()).Returns(studentList);
 
-            var result = controller.GetAll() as OkObjectResult;
+            var result = _controller.GetAll() as OkObjectResult;
             var students = result.Value as List<Student>;
 
             Assert.AreEqual(students.Count, 3);
@@ -53,23 +50,20 @@ public class StudentControllerTest
         [TestMethod]
         public void CanGetStudentById_Ok()
         {
-            var studentLogicMock = new Mock<IStudentLogic>();
             var studentList = new List<Student>
             {
                 new Student() { Id = 1, Name = "Pablo" },
                 new Student() { Id = 2, Name = "Julieta" },
                 new Student() { Id = 3, Name = "Felipe" }
             };
-            studentLogicMock.Setup(service => service.GetStudents()).Returns(studentList);
-            StudentController controller = new StudentController(studentLogicMock.Object);
 
-            studentLogicMock.Setup(service => service.GetStudentById(It.IsAny<int>()))
+            _studentLogicMock.Setup(service => service.GetStudentById(It.IsAny<int>()))
                    .Returns<int>(id => studentList.FirstOrDefault(b => b.Id == id));
 
-            var result1 = controller.GetById(1) as OkObjectResult;
+            var result1 = _controller.GetById(1) as OkObjectResult;
             var student1 = result1.Value as Student;
 
-            var result2 = controller.GetById(2) as OkObjectResult;
+            var result2 = _controller.GetById(2) as OkObjectResult;
             var student2 = result2.Value as Student;
 
             Assert.AreEqual(student1.Name, studentList[0].Name);
@@ -82,19 +76,16 @@ public class StudentControllerTest
         [TestMethod]
         public void CanCreateStudent_Ok()
         {
-            var studentLogicMock = new Mock<IStudentLogic>();
-            var controller = new StudentController(studentLogicMock.Object);
             var student = new Student()
             {
                 Id = 45,
                 Name = "Pablo Emilio Escobar Gaviria",
             };
 
-            var result = controller.Create(student) as OkObjectResult;
+            var result = _controller.Create(student) as OkObjectResult;
 
             Assert.IsNotNull(result);
             Assert.AreEqual(200, result.StatusCode);
         }
-
     }
 }
