c506db8a5c67c8d20c85022ebf6143fe0e0578af
FACULTADES\alumnoFI <aaasss@aaddd>
2023-10-11 09:42:45 -0300

    red - GetStudentById

--- PAC.Tests.WebApi/StudentControllerTest.cs
+++ PAC.Tests.WebApi/StudentControllerTest.cs
@@ -50,5 +50,42 @@ public class StudentControllerTest
             Assert.IsTrue(result.StatusCode.Equals(expected.StatusCode)
                 && expectedStudents.First().Name.Equals(objectResult.First().Name));
         }
+
+        [TestMethod]
+        public void GetStudentById()
+        {
+            //Arrange
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "test"
+            };
+
+            IEnumerable<Student> students = new List<Student>()
+            {
+                student
+            };
+
+            Mock<IStudentLogic> studentLogic = new Mock<IStudentLogic>();
+            studentLogic.Setup(x => x.GetStudents()).Returns(students);
+
+            StudentController studentController = new StudentController(studentLogic.Object);
+            OkObjectResult expected = new OkObjectResult(new List<StudentsDTO>()
+            {
+                new StudentsDTO(students.First())
+            });
+
+            List<StudentsDTO> expectedStudents = (expected.Value as List<StudentsDTO>)!;
+
+            //Act
+            OkObjectResult result = (studentController.GetStudentById() as OkObjectResult)!;
+            Console.WriteLine(result.Value);
+            List<StudentsDTO> objectResult = (result.Value as List<StudentsDTO>)!;
+
+            //Assert
+            studentLogic.VerifyAll();
+            Assert.IsTrue(result.StatusCode.Equals(expected.StatusCode)
+                          && expectedStudents.First().Name.Equals(objectResult.First().Name));
+        }
     }
 }
