0f5a6c8fc4c643f6b920a5c11b2f905675e2caaf
Martin Aristov <50745390+aristovmartin@users.noreply.github.com>
2023-10-11 09:16:32 -0300

    GetOkTest RED

--- PAC.Tests.WebApi/StudentControllerTest.cs
+++ PAC.Tests.WebApi/StudentControllerTest.cs
@@ -18,5 +18,27 @@ public class StudentControllerTest
         public void InitTest()
         {
         }
+
+        [TestMethod]
+        public void GetOkTest()
+        {
+            Student oneStudent = new Student();
+            Student secondStudent = new Student();
+            List<Student> students = new List<Student>();
+            students.Add(oneStudent);
+            students.Add(secondStudent);
+
+            var studentServiceMock = new Mock<IStudentLogic>(MockBehavior.Strict);
+            studentServiceMock.Setup(s => s.Get()).Returns(students);
+            var stuentController = new StudentController(studentServiceMock.Object);
+
+            var result = StudentController.Get();
+            var okResult = result as OkObjectResult;
+            var value = okResult.Value;
+            IEnumerable<Student> valueEnumerable = value as IEnumerable<Student>;
+            List<Student> studentList = valueEnumerable.ToList();
+            studentServiceMock.VerifyAll();
+            Assert.IsTrue(students.SequenceEqual(studentList));
+        }
     }
 }
