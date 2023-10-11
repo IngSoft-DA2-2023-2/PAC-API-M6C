19ba7e9c69e38d6ea8acee1210478290a3057f0e
Matias Praderi <mpraderi.edu@gmail.com>
2023-10-11 09:50:18 -0300

    borrar comment

--- PAC.WebAPI/Filters/AuthorizationFilter.cs
+++ PAC.WebAPI/Filters/AuthorizationFilter.cs
@@ -8,23 +8,6 @@ namespace PAC.WebAPI.Filters
     {
         public virtual void OnAuthorization(AuthorizationFilterContext context)
         {
-/*
-            this._sessionService = context.HttpContext.RequestServices.GetService<ISessionService>();
-            context.HttpContext.Request.Headers.TryGetValue("token", out var stringToken);
-            try
-            {
-                token = Guid.Parse(stringToken);
-                bool tokenIsValid = _sessionService.IsLoggedIn(token);
-                if (!tokenIsValid)
-                {
-                    context.Result = new UnauthorizedResult();
-                }
-
-            }
-            catch (Exception ex)
-            {
-                context.Result = new UnauthorizedResult();
-            }*/
             var authorizationHeader = context.HttpContext.Request.Headers["filter"].ToString();
             if (authorizationHeader != "admin") {
                 context.Result = new UnauthorizedResult();
