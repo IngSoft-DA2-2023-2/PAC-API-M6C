72b120d98fd653ea6c0551d6fb77595d3a00366d
Martin Aristov <50745390+aristovmartin@users.noreply.github.com>
2023-10-11 09:30:54 -0300

    PostOkTest RED
    Created InitTest

--- PAC.Tests.WebApi/StudentControllerTest.cs
+++ PAC.Tests.WebApi/StudentControllerTest.cs
@@ -14,16 +14,26 @@ public class StudentControllerTest
     [TestClass]
     public class UsuarioControllerTest
     {
+        Student oneStudent;
+        Student secondStudent;
         [TestInitialize]
         public void InitTest()
         {
+            oneStudent = new Student()
+            {
+                Id = 1,
+                Name = "Jose"
+            };
+            secondStudent = new Student()
+            {
+                Id = 2,
+                Name = "Pedro"
+            };
         }
 
         [TestMethod]
         public void GetOkTest()
         {
-            Student oneStudent = new Student();
-            Student secondStudent = new Student();
             List<Student> students = new List<Student>();
             students.Add(oneStudent);
             students.Add(secondStudent);
@@ -44,8 +54,6 @@ public class StudentControllerTest
         [TestMethod]
         public void GetIdOkTest()
         {
-            Student oneStudent = new Student();
-
             var studentServiceMock = new Mock<IStudentLogic>(MockBehavior.Strict);
             studentServiceMock.Setup(s => s.GetStudentById(It.IsAny<int>())).Returns(oneStudent);
             var studentController = new StudentController(studentServiceMock.Object);
@@ -53,6 +61,20 @@ public class StudentControllerTest
             var result = studentController.Get(oneStudent.Id);
             var okResult = result as ObjectResult;
             var value = okResult.Value as Student;
+            studentServiceMock.VerifyAll();
+            Assert.AreEqual(value, oneStudent);
+        }
+        [TestMethod]
+        public void PostOkTest()
+        {
+            var studentServiceMock = new Mock<IStudentLogic>(MockBehavior.Strict);
+
+            studentServiceMock.Setup(s => s.InsertStudents(It.IsAny<Student>()));
+            var studentController = new StudentController(studentServiceMock.Object);
+            var result = studentController.Post(oneStudent);
+
+            var createdResult = result as ObjectResult;
+            var value = createdResult.Value;
             studentServiceMock.VerifyAll();
             Assert.AreEqual(value, oneStudent);
         }
