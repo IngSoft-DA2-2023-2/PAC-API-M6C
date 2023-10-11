c8d2ef4ef128aaecf30b2bcdd1461a65e0e03071
Santiago <santiagosalinasoto@gmail.com>
2023-10-11 09:40:17 -0300

    [Red] StudentController

--- PAC.Tests.WebApi/StudentControllerTest.cs
+++ PAC.Tests.WebApi/StudentControllerTest.cs
@@ -7,6 +7,8 @@ using PAC.IBusinessLogic;
 using PAC.Domain;
 using PAC.WebAPI;
 using Microsoft.AspNetCore.Mvc;
+using PAC.IDataAccess;
+using PAC.BusinessLogic;
 
 [TestClass]
 public class StudentControllerTest
@@ -14,9 +16,65 @@ public class StudentControllerTest
     [TestClass]
     public class UsuarioControllerTest
     {
+        private Mock<IStudentLogic> mockStudents;
+        private StudentController studentController;
+
         [TestInitialize]
         public void InitTest()
         {
+            mockStudents = new Mock<IStudentLogic>(MockBehavior.Strict);
+            studentController = new StudentController(mockStudents.Object);
+        }
+
+        [TestMethod]
+        public void GetStudents()
+        {
+            var expectedOutcome = new List<Student>();
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "Santiago"
+            };
+            expectedOutcome.Add(student);
+
+            mockStudents.Setup(s => s.GetStudents()).Returns(expectedOutcome);
+            var result = studentController.GetStudents();
+            var createdResult = result as ActionResult<List<Student>>;
+            Assert.AreEqual(expectedOutcome.Count, createdResult.Value.Count);
+        }
+
+        [TestMethod]
+        public void GetStudentById()
+        {
+            var expectedOutcome = new Student()
+            {
+                Id = 1,
+                Name = "Santiago"
+            };
+
+            mockStudents.Setup(s => s.GetStudentById(1)).Returns(expectedOutcome);
+            var result = studentController.GetStudentById(1);
+            var createdResult = result as ActionResult<Student>;
+            Assert.AreEqual(expectedOutcome, createdResult.Value);
+        }
+
+        [TestMethod]
+        public void InsertStudents()
+        {
+            var student = new Student()
+            {
+                Id = 2,
+                Name = "Santiago"
+            };
+
+            mockStudents.Setup(s => s.GetStudentById(2)).Returns(student);
+            var result = studentController.InsertStudents(student);
+
+            var get = studentController.GetStudentById(2);
+            var createdResult = get as ActionResult<Student>;
+            Assert.AreEqual(student, createdResult.Value);
         }
     }
+
+    
 }
--- PAC.WebAPI/Controllers/StudentController.cs
+++ PAC.WebAPI/Controllers/StudentController.cs
@@ -19,5 +19,23 @@ namespace PAC.WebAPI
         {
             this._studentLogic = studentLogic;
         }
+
+        [HttpGet()]
+        public ActionResult<List<Student>> GetStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Student> GetStudentById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost()]
+        public ActionResult<Student> InsertStudents(Student student)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
